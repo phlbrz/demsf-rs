cf3218c3f8c483c6a785efc7e72b1e037f830cf7
Felipe Campos de Almeida <felipecampos@tjdft.jus.br>
2023-08-08 14:58:01 -0300

    Mods args, download and unzip.

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+/**
+ * DEMSF - Rust - Download and extract Microsoft Sharepoint file
+ * Rewrite of https://github.com/phlbrz/demsf.
+ *
+ * Mod pub.
+ * The mod keyword is used to define a module.
+ * A module is a way to organize code into logical units, and it allows you to group related items together,
+ * such as functions, structs, enums, and other modules.
+ * A module can be defined within a file or across multiple files.
+ *
+ */
+pub mod args;
+pub mod download;
+pub mod unzip;
